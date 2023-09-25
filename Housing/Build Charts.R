f82bfb28533bfa238145a8a559fd3243d16b6150
Spencer Driggs <spencerdriggs@yahoo.com>
2023-09-25 11:53:57 -0600

    start

---
+++ Housing/Build Charts.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(readxl)
+house = read_xlsx('Rent.xlsx',sheet='Rent') %>%
+  mutate(asPercent = Capacity / sum(Capacity) * 100,
+         Owned = str_replace(Owned,'Self|Other','All Others'),
+         Owned =replace_na(Owned, 'All Others'))
+  
+owners = read_xlsx('Rent.xlsx',sheet='Sheet3') %>%
+  filter(!(`Row Labels` %in% c('(blank)','Grand Total','Self'))) %>%
+  arrange(`Sum of Capacity`)
+owners$
+
+
+
+ggplot(owners,aes(x=`Sum of Capacity`,y=`Count of Owned`)) + 
+  geom_point(color='#A13935') +
+  theme_minimal() +
+  labs(x='Total Number of Beds',y='Total Number of Complexes',
+       title='') +
+  geom_text(
+    label=owners$`Row Labels`, 
+    nudge_x = 0.25, nudge_y = 0.25, 
+    check_overlap = T
+  )
+
+ggplot(owners, aes(x=`Sum of Capacity`,y=reorder(`Row Labels`,`Sum of Capacity`,sum))) + geom_col(fill='#A13935')
+  
+ggplot(house, aes(x=Capacity,y=reorder(Owned,Capacity,sum))) + geom_col(fill='#A13935')
+
+
+house2 <- house %>%
+  filter(!(Owned %in% c(NA,'Grand Total','Self')))
+  
+
+ggplot(house, aes(x = reorder(Owned, asPercent,sum), y = asPercent)) +
+  geom_bar(stat = "identity",fill='#A13935') +
+  theme_minimal() +
+  labs(x = "Management Company", y = "",
+       #caption='Limited to companies with >300 capacity',
+       title='Ownership as % of all Student Housing') +
+  coord_flip() +
+  theme(legend.position = "none",
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_blank()
+  ) #+
+  #scale_y_continuous(breaks=c(0,5,10,20,30),labels=c(0,5,10,20,30))
+
+    
+ggplot(house2, aes(x = reorder(Owned, Capacity,sum), y = Capacity)) +
+  geom_bar(stat = "identity",fill='#A13935') +
+  geom_segment(y=4134,yend=4134,x=11.4,xend=10.6) +
+  theme_minimal() +
+  labs(x = "Management Company", y = "Total Capacity",
+       caption='Limited to companies with >300 capacity',
+       title='Who Owns the Most Apartments?',
+       sub='Redstone + Rexburg Housing Have Huge Market Lead') +
+  coord_flip() +
+  theme(legend.position = "none",
+        panel.grid.major.y = element_blank()) +
+  annotate("text",y=4100,x=9.6, size=3,label="Addition of\nRexburg Housing")
+
